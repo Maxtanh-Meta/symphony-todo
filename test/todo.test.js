c0670eaf71abbe264ec3c05259b6c67c4a2653fa
symphony-go <noreply@local>
2026-05-13 23:07:28 -0700

    Implement issue #25

--- test/todo.test.js
+++ test/todo.test.js
@@ -2,6 +2,7 @@ const TodoApp = require('../src/todo');
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
+const indexHtml = fs.readFileSync(path.join(__dirname, '..', 'public', 'index.html'), 'utf8');
 
 let passed = 0, failed = 0;
 
@@ -11,6 +12,9 @@ function assert(condition, msg) {
 }
 
 console.log('TodoApp Tests\n');
+
+assert(indexHtml.includes('--completed-label-bg: #dcfce7;'), 'completed label uses a light green background');
+assert(indexHtml.includes("return completed ? '<span class=\"completed-label\">Completed</span>' : '';"), 'completed label renders only for completed todos');
 
 const tempDir = fs.mkdtempSync(path.join(os.tmpdir(), 'todo-test-'));
 const storagePath = path.join(tempDir, 'todos.json');
